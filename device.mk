0cc45701f62b4cec0889cb2794dbb220cee706d5
HuaLiMao-AQ <hualimao_aq@163.com>
2022-03-06 23:09:38 +0800

    Initial atom tree Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

---
+++ device.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2022 The ArrowOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/atom
+
+# Call proprietary blob setp
+$(call inherit-product, device/xiaomi/mt6873-common/common.mk)
+
+# Interfaces
+include $(LOCAL_PATH)/vendor_prop.mk
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6873:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6873
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.mt6873.rc \
+    init.mt6873.usb.rc \
+    fstab.mt6873 \
+    ueventd.mt6873.rc \
